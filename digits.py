f6b3c865b5feed7aff8115d33847c8e46a26fcbb
Andre Perkins <perki128@mail.chapman.edu>
2016-03-17 20:00:20 -0700

    Added files via upload

---
+++ digits.py
@@ -0,0 +1,14 @@
+print """
+ File: <histogram.py>
+
+Copyright (c) 2016 <Andre Perkins>
+
+License: MIT
+
+Ls to represent a list. We define bar to attach a string to star so that
+ for the values inside the list will be represented by stars
+"""
+user_input = raw_input("Enter Any Positive Integer:")
+def numbers(n):
+    return len(str(n))
+print numbers(user_input)
