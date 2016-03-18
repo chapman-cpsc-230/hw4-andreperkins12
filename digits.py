d945dfe7eaac5099d21bdeb1e024dd2bd9c501c8
Andre Perkins <perki128@mail.chapman.edu>
2016-03-17 20:04:16 -0700

    Update digits.py

--- digits.py
+++ digits.py
@@ -5,7 +5,7 @@ Copyright (c) 2016 <Andre Perkins>
 
 License: MIT
 
-
+Machine printing the amount of numbers in the input given by the user
 """
 user_input = raw_input("Enter Any Positive Integer:")
 def numbers(n):
